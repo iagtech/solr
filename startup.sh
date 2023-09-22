5dcbea3d3c3362e45700dd23314b6ca2819e212c
Ethan McGee <bulletshot60@users.noreply.github.com>
2023-09-22 12:52:54 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,9 +4,11 @@ CORES="${CORES:-sample}"
 
 for i in $(echo $CORES | tr "," "\n")
 do
-  cp -r example-core /tmp/$i
-  sed -i "s/EXAMPLE/$i/g" /tmp/$i/core.properties
-  cp -r /tmp/$i /var/solr/data/$i
+  if [ ! -d "/var/solr/data/$i" ]; then
+    cp -r example-core /tmp/$i
+    sed -i "s/EXAMPLE/$i/g" /tmp/$i/core.properties
+    cp -r /tmp/$i /var/solr/data/$i
+  fi
 done
 
 cp solr.xml /var/solr/data/solr.xml
